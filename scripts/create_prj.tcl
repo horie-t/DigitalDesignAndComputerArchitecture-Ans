e6c13bb29838bfaca0be86df3a0ee3cfac1d6522
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-29 14:52:22 +0900

    プロジェクト作成時に、制約ファイルをインポートするように修正

--- scripts/create_prj.tcl
+++ scripts/create_prj.tcl
@@ -20,6 +20,30 @@ if { $argc == 0 } {
     exit 1
 }
 
+# Vivadoプロジェクトを作成
 set project_name [lindex $argv 0]
-
 create_project $project_name $project_dir -part $part_name
+
+#
+# 制約ファイルをインポート
+# 
+set proj_dir [get_property directory [current_project]]
+
+# Create 'constrs_1' fileset (if not found)
+if {[string equal [get_filesets -quiet constrs_1] ""]} {
+  create_fileset -constrset constrs_1
+}
+
+# Set 'constrs_1' fileset object
+set obj [get_filesets constrs_1]
+
+# Add/Import constrs file and set constrs file properties
+set file "[file normalize "$proj_dir/../srcs/constrs/Nexys4DDR.xdc"]"
+set file_added [add_files -norecurse -fileset $obj $file]
+set file "$proj_dir/../srcs/constrs/Nexys4DDR.xdc"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
+set_property -name "file_type" -value "XDC" -objects $file_obj
+
+# Set 'constrs_1' fileset properties
+set obj [get_filesets constrs_1]
