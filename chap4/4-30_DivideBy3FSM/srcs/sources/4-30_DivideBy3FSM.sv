fd43825a86438adfbb26ce25636a670ea1e8c1e0
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2017-11-30 16:58:22 +0900

    クロック発生用ボタンを追加

--- chap4/4-30_DivideBy3FSM/srcs/sources/4-30_DivideBy3FSM.sv
+++ chap4/4-30_DivideBy3FSM/srcs/sources/4-30_DivideBy3FSM.sv
@@ -1,4 +1,23 @@
 /**
+ * @brief トップ・モジュール
+ */
+module topModule(input logic clk,
+		 input logic  reset,
+		 input logic  manualClk,
+		 output logic y);
+
+   /*
+    * チャタリング・防止の出力を擬似的なクロック信号にしてから、
+    * FSMに渡す。
+    */
+   logic pseudoClk;
+   eliminateChatter eliCht(clk, reset, manualClk, pseudoClk);
+   
+   divideby3FSM fsm(pseudoClk, reset, y);
+
+endmodule // topModule
+
+/**
  * @brief 3進有限状態マシン
  */
 module divideby3FSM(input logic clk,
@@ -26,3 +45,55 @@ module divideby3FSM(input logic clk,
    assign y = (state == S0);
 
 endmodule // divideby3FSM
+
+/**
+ * 簡易的なチャタリング防止回路
+ */
+module eliminateChatter(input logic clk,
+			input logic  reset,
+			input logic  a,
+			output logic y);
+   /*
+    * このやり方は、あくまで簡易的なもの。欠点が以下の通り存在する。
+    * ・加算回路を内部に持つので、規模が大きくなる。
+    * ・enableの周期で論理が動くので応答が遅れがち(指の動きよりは圧倒的に速いが)
+    * ・enableの間隔以下の動きも拾ってしまう事があるので、チャタリング
+    *   防止としては、完全ではない。
+    * 
+    * 本当は、分周した遅いクロックをシステムからもらって、
+    * シフトレジスタを使って、一定時間、入力の変化が連続
+    * してから応答するようにした方がよい。
+    */
+
+   /*
+    * 約30Hzのパルスを作る。
+    * (15'h7FFF(32767)までカウント。100MHzで動かすと約32msで一周する)
+    */
+   logic [14:0] count;
+   logic 	enable;
+   always_ff @(posedge clk, posedge reset)
+     if (reset) count = 14'b0;
+     else count = count + 1;
+
+   always_comb
+     enable = (count == 15'h7FFF);
+
+   logic 	ff1, ff2;
+   always_ff @(posedge clk, posedge reset)
+     if (reset)
+       begin
+	  ff1 <= 1'b0;
+	  ff2 <= 1'b0;
+       end
+     else if (enable)
+       begin
+	  ff1 <= a;
+	  ff2 <= ff1;
+       end
+
+   // ff1 & ff2で立ち上がりを検出し、
+   // enableとの論理和で1クロック分の信号になる。
+   always_comb
+     y = ff1 & ~ff2 & enable;
+
+endmodule // eliminateChatter
