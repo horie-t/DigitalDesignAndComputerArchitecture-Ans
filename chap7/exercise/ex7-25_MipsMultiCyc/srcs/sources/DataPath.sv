59ea764ece032d7d2315a1b1f38950fe18ab57a2
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2018-01-11 17:44:11 +0900

    演算結果7.25 メモリ読み書きのデータパスを作成

--- chap7/exercise/ex7-25_MipsMultiCyc/srcs/sources/DataPath.sv
+++ chap7/exercise/ex7-25_MipsMultiCyc/srcs/sources/DataPath.sv
@@ -4,12 +4,14 @@
 module DataPath
   (input logic clk, reset, 	// クロック、リセット
    output logic [31:0] memoryAddress, // メモリアドレス
+   input logic         instrOrDataAddress, // 命令アドレスか、データアドレスか
    input logic 	       instrReadEnable, // 命令読み込みイネーブル(イネーブル時にreadDataが命令として扱われる)
    input logic [31:0]  readData, // メモリ読み込み値
    input logic [2:0]   aluControl,// ALU制御
    output logic        aluResultZero); // ALU出力ゼロ
-
-   logic [31:0] pc, pcNext, instr; // プログラム・カウンタ値(PC)、次のPC、命令値
+   
+   logic [31:0] pc, pcNext; // プログラム・カウンタ値(PC)、次のPC、
+   logic [31:0] instr, memoryLoadData;		    // 命令値、メモリ・ロード・データ
    logic [31:0] regFileData1, regFileData2; // レジスタ・ファイル読み込み値1, 2
    logic [31:0] rfData1, rfData2;	    // レジスタ・ファイル読み込み値1, 2(デコード・フェーズ)
    logic [31:0] instrSignImmediate;	    // 命令の符号拡張即値
@@ -18,9 +20,10 @@ module DataPath
    // 命令フェッチ
    FlopReset #(32) programCounterReg(clk, reset, pcNext, pc); // プログラム・カウンタ・レジスタ
    
-   assign memoryAddress = pc;	// TMP
+   assign memoryAddress = instrOrDataAddress ? pc : aluOut;
    
    FlopEnable #(32) instrReg(clk, reset, instrReadEnable, readData, instr);
+   FlopReset #(32) memDataReg(clk, reset, readData, memoryLoadData);
 
    // デコード
    RegisterFile regFile(clk, regFileWriteEnable, 
